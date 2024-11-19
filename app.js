21c3e27e847726e5924b7931259ec6bd6aeb4a9f
mdnumans2024 <numanshorif2020@gmail.com>
2024-11-18 19:02:12 -0700

    1st commit

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || 8000;
+
+app.use(express.json());
+
+app.use((req, res, next)=>{
+    console.log(`Request received : ${req.method}, ${req.url}`);
+    next();
+
+})
+
+app.get("/user/:id",(req,res) => {
+    const userId = req.params.id ; // dot notation
+    res.send(`User id is${userId}`);
+});
+
+app.get("/search",(req,res) => {
+    const query =req.query.q;
+    res.send (`Search query is: ${query}`);
+});
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
